575f50aa2ca348d95ee40391708b42829c0094a6
netero1mh <123423101+netero1mh@users.noreply.github.com>
2023-05-22 05:32:32 +0300

    Update tag.js

--- plugins/tag.js
+++ plugins/tag.js
@@ -8,7 +8,7 @@ const {getString} = require('./misc/lang');
 const {readFileSync} = require('fs');
 const {saveMessage} = require('./misc/saveMessage');
 const Lang = getString('group');
-Module({pattern: 'tag ?(.*)',use: 'group', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
+Module({pattern: 'منشن ?(.*)',use: 'group', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
 if (match[1] === "all" || (match[1] && match[1].startsWith("ad")) || !message.reply_message) return;
 var target = message.jid
 if (match[1] && /[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g.test(match[1])) target = [...match[1].match(/[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g)];
